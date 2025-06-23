fe8f5a797e22b7541e53e1da84c0e8767b2127e8
chrleroy <you@example.com>
2025-06-23 14:01:34 +0200

    init

---
+++ includes/minirt.h
@@ -0,0 +1,11 @@
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include "standardHeaders.c"
+# include "utilities.h"
+# include "parsing.h"
+# include "execution.h"
+
+int	main(int argc, char **argv, char **envp);
+
+#endif
---
+++ includes/standardHeaders.h
@@ -0,0 +1,8 @@
+#ifndef STANDARDHEADERS_H
+# define STANDARDHEADERS_H
+
+# include <stdlib.h>
+# include <unistd.h> 
+# include <maths.h>
+
+#endif
