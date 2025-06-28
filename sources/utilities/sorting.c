703fd785949969d9910e1a55028bd9e5f3dcc640
chrleroy <you@example.com>
2025-06-28 17:40:45 +0200

    No leaks

---
+++ sources/utilities/sorting.c
@@ -0,0 +1,98 @@
+#include "minirt.h"
+
+//
+static void	maxHeapify(int *arr, int size, int i)
+{
+	int	temp;
+	int	left;
+	int	right;
+	int	largest;
+
+    largest = i;
+    left = 2 * i + 1;
+    right = 2 * i + 2;
+    if (left < size && arr[left] > arr[largest])
+        largest = left;
+    if (right < size && arr[right] > arr[largest])
+        largest = right;
+    if (largest != i)
+    {
+		temp = arr[i];
+		arr[i] = arr[largest];
+		arr[largest] = temp;
+        maxHeapify(arr, size, largest);
+    }
+}
+
+//
+static void	minHeapify(int *arr, int size, int i)
+{
+	int	temp;
+	int	left;
+	int	right;
+	int	smallest;
+
+    smallest = i;
+    left = 2 * i + 1;
+    right = 2 * i + 2;
+    if (left < size && arr[left] < arr[smallest])
+        smallest = left;
+    if (right < size && arr[right] < arr[smallest])
+        smallest = right;
+    if (smallest != i)
+    {
+		temp = arr[i];
+		arr[i] = arr[smallest];
+		arr[largest] = temp;
+        minHeapify(arr, size, smallest);
+    }
+}
+
+
+//
+static void	buildMinHeap(int *arr, int size)
+{
+	int i;
+
+	i = size << 1 - 1;
+	while (i--)
+		minHeapify(arr, size, i);
+}
+
+//
+static void	buildMaxHeap(int *arr, int size)
+{
+	int i;
+
+	i = size << 1 - 1;
+	while (i--)
+		maxHeapify(arr, size, i);
+}
+
+//Applies heap sort on the array based on the boolean scenario
+int	*heapSort(int *arr, int size, bool type)
+{
+	int		i;
+	void	(*builder)(int *, int);
+	void	(*sorter)(int *, int, int);
+
+	i = size - 1;
+	if (type)
+	{
+		builder = buildMaxHeap;
+		sorter = maxHeapify;
+	}
+	else
+	{
+		builder = buildMinHeap;
+		sorter = minHeapify;
+	}
+	builder(arr, size);
+	while (i > 0)
+	{
+       	swap(&arr[0], &arr[i]);
+		sorter(arr, i, 0);
+		i--;
+	}
+	return (arr);
+}
