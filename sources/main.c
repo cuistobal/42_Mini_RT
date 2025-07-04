5a5c4bad3d5751dfba75332a848d10559e42dcab
chrleroy <you@example.com>
2025-07-04 11:44:45 +0200

    print rendering && start tests

--- sources/main.c
+++ sources/main.c
@@ -7,6 +7,8 @@ int	main(int argc, char **argv, char **envp)
 
 	if (!parsing(argc, argv, envp, &minirt))
 		return (print_minirt(minirt), 1);
+	minirt.width = 800;
+	minirt.height = 1200;
 	start_rendering(&minirt);
 	return (free_minirt(&minirt), 0);
 }
