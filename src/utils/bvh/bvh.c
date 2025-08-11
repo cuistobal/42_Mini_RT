62e069a9cdefb37eed91ebd3a40f506ab70daf92
chrleroy <you@example.com>
2025-08-11 10:37:11 +0200

    Profiling report

--- src/utils/bvh/bvh.c
+++ src/utils/bvh/bvh.c
@@ -6,7 +6,7 @@
 /*   By: cuistobal <cuistobal@student.42.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 00:00:00 by cuistobal        #+#    #+#             */
-/*   Updated: 2025/08/09 16:52:38 by chrleroy         ###   ########.fr       */
+/*   Updated: 2025/08/11 10:36:34 by chrleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,72 +84,6 @@ static t_aabb	aabb_union(t_aabb a, t_aabb b)
 	result.max.y = fmax(a.max.y, b.max.y);
 	result.max.z = fmax(a.max.z, b.max.z);	
 	return (result);
-}
-
-/*
-** intersect_aabb - Fast ray-AABB intersection test
-** Returns 1 if ray intersects the bounding box, 0 otherwise
-** We need to split that shit for norminette
-*/
-static int	intersect_aabb(t_ray ray, t_aabb box)
-{
-	double	t_min;
-	double	t_max;
-	double	t1;
-	double	t2;
-	double	temp;
-	int		i;
-	double	ray_dir[3];
-	double	ray_orig[3];
-	double	box_min[3];
-	double	box_max[3];
-
-	// Convert to arrays for easier iteration
-	ray_dir[0] = ray.direction.x;
-	ray_dir[1] = ray.direction.y;
-	ray_dir[2] = ray.direction.z;
-	ray_orig[0] = ray.origin.x;
-	ray_orig[1] = ray.origin.y;
-	ray_orig[2] = ray.origin.z;
-	box_min[0] = box.min.x;
-	box_min[1] = box.min.y;
-	box_min[2] = box.min.z;
-	box_max[0] = box.max.x;
-	box_max[1] = box.max.y;
-	box_max[2] = box.max.z;	
-	t_min = -INFINITY;
-	t_max = INFINITY;
-
-	// Test intersection with each pair of parallel planes
-	i = 0;
-	while (i < 3)
-	{
-		if (fabs(ray_dir[i]) < EPSILON)
-		{
-			// Ray is parallel to the slab
-			if (ray_orig[i] < box_min[i] || ray_orig[i] > box_max[i])
-				return (0);
-		}
-		else
-		{
-			t1 = (box_min[i] - ray_orig[i]) / ray_dir[i];
-			t2 = (box_max[i] - ray_orig[i]) / ray_dir[i];
-			if (t1 > t2)
-			{
-				temp = t1;
-				t1 = t2;
-				t2 = temp;
-			}	
-			if (t1 > t_min)
-				t_min = t1;
-			if (t2 < t_max)
-				t_max = t2;
-			if (t_min > t_max)
-				return (0);
-		}
-		i++;
-	}
-	return (t_max > EPSILON);
 }
 
 /*
@@ -240,84 +174,6 @@ t_bvh_node	*build_bvh(t_scene *scene)
 	}
 	root = build_bvh_recursive(object_array, count);	
 	return (safe_free((void **)&object_array), root);
-}
-
-/*
-** intersect_bvh - Traverse BVH to find closest intersection
-*/
-int	intersect_bvh(t_ray ray, t_bvh_node *node, t_hit *hit)
-{
-	t_hit	left_hit;
-	t_hit	right_hit;
-	int		left_intersect;
-	int		right_intersect;
-	double	t;
-
-	if (!node || !hit)
-		return (0);
-	
-	// Test ray against bounding box first
-	if (!intersect_aabb(ray, node->bounds))
-		return (0);
-	
-	// Leaf node - test against object
-	if (node->object)
-	{
-		if (node->object->type == SPHERE)
-			t = intersect_sphere(ray, node->object);
-		else if (node->object->type == PLANE)
-			t = intersect_plane(ray, node->object);
-		else if (node->object->type == CYLINDER)
-			t = intersect_cylinder(ray, node->object);
-		else if (node->object->type == CONE)
-			t = intersect_cone(ray, node->object);
-		else if (node->object->type == CUBE)
-			t = intersect_cube(ray, node->object);
-		else
-			t = -1.0;
-		
-		if (t > 0)
-		{
-			hit->t = t;
-			hit->point = ray_at(ray, t);
-			hit->normal = get_object_normal(hit->point, node->object);
-			hit->material = &node->object->material;
-			hit->object = node->object;
-			return (1);
-		}
-		return (0);
-	}
-	
-	// Internal node - test both children
-	left_intersect = 0;
-	right_intersect = 0;
-	
-	if (node->left)
-		left_intersect = intersect_bvh(ray, node->left, &left_hit);
-	if (node->right)
-		right_intersect = intersect_bvh(ray, node->right, &right_hit);
-	
-	// Return closest intersection
-	if (left_intersect && right_intersect)
-	{
-		if (left_hit.t < right_hit.t)
-			*hit = left_hit;
-		else
-			*hit = right_hit;
-		return (1);
-	}
-	else if (left_intersect)
-	{
-		*hit = left_hit;
-		return (1);
-	}
-	else if (right_intersect)
-	{
-		*hit = right_hit;
-		return (1);
-	}
-	
-	return (0);
 }
 
 /*
