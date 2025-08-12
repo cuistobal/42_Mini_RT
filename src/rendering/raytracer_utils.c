272915e743b9b8c2c1487f9cda3ae6dbacb5926e
chrleroy <you@example.com>
2025-08-12 15:07:59 +0200

    Ajout des flags d optimisation [STABLE]

--- src/rendering/raytracer_utils.c
+++ src/rendering/raytracer_utils.c
@@ -30,20 +30,13 @@ void	calculate_viewport_dimensions(t_minirt *rt, t_camera *camera,
 /*
 ** calculate_pixel_world_position - Convert screen coords to world position
 */
-t_vec3	calculate_pixel_world_position(t_camera *camera, double u, \
-		double v, double half_width, double half_height)
+t_vec3	calculate_pixel_world_position(t_camera *c, double u, double v, double hw, double hh)
 {
-	t_vec3	pixel_world;
-	t_vec3	right_offset;
-	t_vec3	up_offset;
-
-	pixel_world = camera->position;
-	pixel_world = vec3_add(pixel_world, camera->direction);
-	right_offset = vec3_mult(camera->right, (u - 0.5) * 2.0 * half_width);
-	up_offset = vec3_mult(camera->up, (0.5 - v) * 2.0 * half_height);
-	pixel_world = vec3_add(pixel_world, right_offset);
-	pixel_world = vec3_add(pixel_world, up_offset);
-	return (pixel_world);
+	return vec3_add(
+		vec3_add(
+			vec3_add(c->position, c->direction),
+			vec3_mult(c->right, (u - 0.5) * 2.0 * hw)),
+		vec3_mult(c->up, (0.5 - v) * 2.0 * hh));
 }
 
 /*
@@ -51,11 +44,11 @@ t_vec3	calculate_pixel_world_position(t_camera *camera, double u, \
 */
 t_color	calculate_background_color(t_ray ray)
 {
-	t_color	background_color;
-
-	background_color = color_new((int)(135 + ray.direction.y * 50), (int)(206 \
-				+ ray.direction.y * 49), 255);
-	return (color_clamp(background_color));
+	   return color_clamp((t_color){
+			   (int)(135 + ray.direction.y * 50),
+			   (int)(206 + ray.direction.y * 49),
+			   255
+	   });
 }
 
 /*
@@ -64,14 +57,7 @@ t_color	calculate_background_color(t_ray ray)
 void	render_pixel_at_coordinates(t_minirt *rt, int x, int y,
 	double inv_width, double inv_height)
 {
-	double	u;
-	double	v;
-	t_ray	ray;
-	t_color	pixel_color;
-
-	u = (double)x * inv_width;
-	v = (double)y * inv_height;
-	ray = get_camera_ray(rt, &rt->scene.camera, u, v);
-	pixel_color = raycast(ray, &rt->scene, MAX_DEPTH);
-	put_pixel(&rt->mlx, x, y, pixel_color);
+	   put_pixel(&rt->mlx, x, y, raycast(
+					   get_camera_ray(rt, &rt->scene.camera, (double)x * inv_width, (double)y * inv_height),
+					   &rt->scene, MAX_DEPTH));
 }
