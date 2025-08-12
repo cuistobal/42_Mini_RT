2b5cd4bd275856522065f0973aa7b12d44c04262
chrleroy <you@example.com>
2025-08-12 13:19:53 +0200

    Optimisationd es intersections [STABLE]

--- src/rendering/raycast_optimized.c
+++ src/rendering/raycast_optimized.c
@@ -20,70 +20,11 @@ t_color	calculate_hit_color(t_ray ray, t_hit *hit, t_scene *scene, int depth)
 	t_color	base_color;
 	t_color	reflection_color;
 	t_color	refraction_color;
-	t_color	final_color;
 
 	base_color = calculate_lighting(hit->point, hit->normal, scene,
 			hit->material);
 	reflection_color = calculate_reflection(ray, hit, scene, depth);
 	refraction_color = calculate_refraction(ray, hit, scene, depth);
-	final_color = color_add(base_color, reflection_color);
-	final_color = color_add(final_color, refraction_color);
-	return (color_clamp(final_color));
+	return color_clamp(color_add(color_add(base_color, reflection_color), \
+		refraction_color));
 }
-
-// /*
-// ** add_reflection_if_needed - Add reflection to base color if material has it
-// */
-// static t_color	add_reflection_if_needed(t_color base_color, t_ray ray,
-// 	t_hit *hit, t_scene *scene, int depth)
-// {
-// 	t_color	reflection_color;
-
-// 	if (hit->material->reflection > 0.01 && depth > 1)
-// 	{
-// 		reflection_color = calculate_reflection(ray, hit, scene, depth);
-// 		base_color = color_add(base_color,
-// 				color_mult(reflection_color, hit->material->reflection));
-// 	}
-// 	return (base_color);
-// }
-
-// /*
-// ** add_refraction_if_needed - Add refraction to base color if material has it
-// */
-// static t_color	add_refraction_if_needed(t_color base_color, t_ray ray,
-// 	t_hit *hit, t_scene *scene, int depth)
-// {
-// 	t_color	refraction_color;
-
-// 	if (hit->material->transparency > 0.01 && depth > 1)
-// 	{
-// 		refraction_color = calculate_refraction(ray, hit, scene, depth);
-// 		base_color = color_add(base_color,
-// 				color_mult(refraction_color, hit->material->transparency));
-// 	}
-// 	return (base_color);
-// }
-
-// /*
-// ** raycast_optimized - Optimized ray casting with early termination
-// */
-// t_color	raycast_optimized(t_ray ray, t_scene *scene, int depth)
-// {
-// 	t_hit	hit;
-// 	t_color	base_color;
-
-// 	if (!scene || depth <= 0)
-// 		return (color_new(0, 0, 0));
-// 	if (intersect_scene(ray, scene, &hit))
-// 	{
-// 		base_color = calculate_lighting(hit.point, hit.normal, scene,
-// 				hit.material);
-// 		base_color = add_reflection_if_needed(base_color, ray, &hit,
-// 				scene, depth);
-// 		base_color = add_refraction_if_needed(base_color, ray, &hit,
-// 				scene, depth);
-// 		return (color_clamp(base_color));
-// 	}
-// 	return (color_new(135, 206, 255));
-// }
