21b3c06ad84fc1a595cd37babc179a67bb6c18ac
chrleroy <you@example.com>
2025-08-18 10:21:19 +0200

    aabb intersection [STABLE]

--- src/utils/aabb_intersections.c
+++ src/utils/aabb_intersections.c
@@ -6,7 +6,7 @@
 /*   By: chrleroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 09:44:17 by chrleroy          #+#    #+#             */
-/*   Updated: 2025/08/16 09:05:36 by chrleroy         ###   ########.fr       */
+/*   Updated: 2025/08/18 10:20:42 by chrleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,20 @@
 
 static inline void	swap(double *t0, double *t1)
 {
-    double	tmp;
+	double	tmp;
 
-    tmp = *t0;
-    *t0 = *t1;
-    *t1 = tmp;
+	tmp = *t0;
+	*t0 = *t1;
+	*t1 = tmp;
+}
+
+static int	helper(double *tmin, double *tmax, double t0, double t1)
+{
+	if (t0 > *tmin)
+		*tmin = t0;
+	if (t1 < *tmax)
+		*tmax = t1;
+	return (*tmin > *tmax);
 }
 
 /*
@@ -26,41 +35,31 @@ static inline void	swap(double *t0, double *t1)
 ** Returns 1 if ray intersects the bounding box, 0 otherwise
 ** We need to split that shit for norminette
 */
-
 int	intersect_aabb_query(t_aabb_query *q)
 {
-    int (i) = 0;
-    double	(tmin), (tmax), (t0), (t1), (invD), (orig), (dir), (bmin), bmax;
-    tmin = -INFINITY;
-    tmax = INFINITY;
-    while (i < 3)
-    {
-        orig = ((double *)&q->origin)[i];
-        dir = ((double *)&q->dir)[i];
-        bmin = ((double *)&q->box.min)[i];
-        bmax = ((double *)&q->box.max)[i];
-        if (fabs(dir) < EPSILON)
-        {
-            if (orig < bmin || orig > bmax)
-                return (0);
-        }
-        else
-        {
-            invD = 1.0 / dir;
-            t0 = (bmin - orig) * invD;
-            t1 = (bmax - orig) * invD;
-            if (invD < 0.0)
-                swap(&t0, &t1);
-            if (t0 > tmin)
-                tmin = t0;
-            if (t1 < tmax)
-                tmax = t1;
-            if (tmin > tmax)
-                return (0);
-        }
+	int (i) = 0;
+	double (tmin), (tmax), (t0), (t1), (invD), (orig), (dir), (bmin), (bmax);
+	tmax = INFINITY;
+	tmin = -INFINITY;
+	while (i < 3)
+	{
+		dir = ((double *)&q->dir)[i];
+		orig = ((double *)&q->origin)[i];
+		bmin = ((double *)&q->box.min)[i];
+		bmax = ((double *)&q->box.max)[i];
+		if (fabs(dir) < EPSILON && (orig < bmin || orig > bmax))
+			return (0);
+		else if (fabs(dir) >= EPSILON)
+		{
+			invD = 1.0 / dir;
+			t0 = (bmin - orig) * invD;
+			t1 = (bmax - orig) * invD;
+			if (invD < 0.0)
+				swap(&t0, &t1);
+			if (helper(&tmin, &tmax, t0, t1))
+				return (0);
+		}
 		i++;
-    }
-    q->tmin = tmin;
-    q->tmax = tmax;
-    return (tmax > EPSILON);
+	}
+	return (q->tmin = tmin, q->tmax = tmax, tmax > EPSILON);
 }
