f640f33cd395d959174669c5fef9f810d17c66d3
chrleroy <you@example.com>
2025-08-30 08:58:46 +0200

    Normaj parsing

--- src/parsing/parse_vec3.c
+++ src/parsing/parse_vec3.c
@@ -6,11 +6,27 @@
 /*   By: chrleroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 08:41:33 by chrleroy          #+#    #+#             */
-/*   Updated: 2025/08/30 08:48:09 by chrleroy         ###   ########.fr       */
+/*   Updated: 2025/08/30 08:51:20 by chrleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minirt.h"
+
+static int	loop_helper(char **token, double *value)
+{
+	char	*next;
+
+	next = strchr(*token, ',');
+	if (next)
+		*next = '\0';
+	if (!parse_double(*token, value))
+		return (1);
+	if (next)
+		*token = next + 1;
+	else
+		*token = NULL;
+	return (0);
+}
 
 /*
 ** parse_vec3 - Parse a 3D vector from string (format: x,y,z)
@@ -22,27 +38,19 @@ int	parse_vec3(char *str, t_vec3 *result)
 {
 	char	*str_copy;
 	char	*token;
-	char	*next;
 	double	values[3];
 	int		i;
 
 	if (!str || !result)
 		return (0);
+	i = 0;
 	str_copy = safe_strdup(str);
 	str_copy = skip_whitespace(str_copy);
-	i = 0;
 	token = str_copy;
 	while (i < 3 && token)
 	{
-		next = strchr(token, ',');
-		if (next)
-			*next = '\0';
-		if (!parse_double(token, &values[i]))
+		if (loop_helper(&token, &values[i]))
 			return (safe_free((void **)&str_copy), 0);
-		if (next)
-			token = next + 1;
-		else
-			token = NULL;
 		i++;
 	}
 	safe_free((void **)&str_copy);
