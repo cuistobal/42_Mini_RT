3f6207049732f229315646aa8886497b49f08521
chrleroy <you@example.com>
2025-09-02 15:27:32 +0200

    stable

--- src/rendering/raycast_optimized.c
+++ src/rendering/raycast_optimized.c
@@ -6,7 +6,7 @@
 // /*   By: cuistobal <cuistobal@student.42.fr>       +#+  +:+       +#+        */
 // /*                                                +#+#+#+#+#+   +#+           */
 // /*   Created: 2025/01/28 00:00:00 by cuistobal        #+#    #+#             */
-/*   Updated: 2025/09/02 15:14:52 by chrleroy         ###   ########.fr       */
+/*   Updated: 2025/09/02 15:25:16 by chrleroy         ###   ########.fr       */
 // /*                                                                            */
 // /* ************************************************************************** */
 
@@ -15,26 +15,28 @@
 /*
 ** calculate_hit_color - Calculate color for a ray-object intersection
 */
-// t_color	calculate_hit_color(t_ray ray, t_hit *hit, t_scene *scene, int depth)
-// {
-// 	t_color	base_color;
-// 	t_color	reflection_color;
-// 	t_color	refraction_color;
+/*
+ t_color	calculate_hit_color(t_ray ray, t_hit *hit, t_scene *scene, int depth)
+ {
+ 	t_color	base_color;
+ 	t_color	reflection_color;
+ 	t_color	refraction_color;
 
-// 	base_color = calculate_lighting(hit->point, hit->normal, scene,
-// 			hit->material);
-// 	reflection_color = calculate_reflection(ray, hit, scene, depth);
-// 	refraction_color = calculate_refraction(ray, hit, scene, depth);
-// 	return (color_add(color_add(base_color, reflection_color), refraction_color));
-// }
-
+ 	base_color = calculate_lighting(hit->point, hit->normal, scene,
+ 			hit->material);
+ 	reflection_color = calculate_reflection(ray, hit, scene, depth);
+ 	refraction_color = calculate_refraction(ray, hit, scene, depth);
+ 	return (color_add(color_add(base_color, reflection_color), refraction_color));
+ }
+*/
 t_color calculate_hit_color(t_ray ray, t_hit *hit, t_scene *scene, int depth)
 {
     t_color base_color;
     t_color reflection_color;
     t_color refraction_color;
 
-    int scale = 1;
+    int scale = 16;
+
     int x = (int)floor(hit->point.x / scale);
     int y = (int)floor(hit->point.y / scale);
     int z = (int)floor(hit->point.z / scale);
@@ -50,3 +52,4 @@ t_color calculate_hit_color(t_ray ray, t_hit *hit, t_scene *scene, int depth)
 
     return color_add(color_add(base_color, reflection_color), refraction_color);
 }
+
